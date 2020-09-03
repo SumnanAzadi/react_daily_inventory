33e61dfc2d3fcfaadab547beea2d7a74ce262e20
SumnanAzadi <anadaplay@gmail.com>
2020-09-03 20:05:09 +0600

    React event binding

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,20 +1,32 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-  //render: what DOM elements do I render to the html page.
+  myInput = React.createRef();
+  /* handleClick() {
+    alert("Hey Button Clicked !!!");
+  } */
+  gotoStore = (e) => {
+    // 1. Stop the form from submitting
+    e.preventDefault();
+    // 2. get the text from that input
+    const storeName = this.myInput.current.value;
+    // 3. Change the page to /store/whatever-they-entered
+    console.log(`Going to Store`);
+  };
   render() {
-    /* can't return sibling elements.
-    Workaround:
-    <React.Fragment>
-    <p></p>
-    <h1></h1>
-    </React.Fragment> */
-
-    //By Default only return one parent elements, can have multiple child elements of that parent elements
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.gotoStore}>
         <h2>Please Enter a store</h2>
-        <input type="text" required placeholder="Enter your Store name" />
+        {/* <button onClick={this.handleClick}>Click me !!!</button> */}
+        {/* to reference input field we have "ref" */}
+        <input
+          type="text"
+          required
+          ref={this.myInput}
+          placeholder="Enter your Store name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store</button>
       </form>
     );
