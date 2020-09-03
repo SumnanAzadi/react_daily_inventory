058a22d71b545530f71d5eb69770762656ecc8a5
SumnanAzadi <anadaplay@gmail.com>
2020-09-03 10:11:47 +0600

    First Basic JSX in StorePicker

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,9 +1,23 @@
 import React from "react";
 
 class StorePicker extends React.Component {
-  //render: what DOM elements do I render to the html page
+  //render: what DOM elements do I render to the html page.
   render() {
-    return <p>Hello from first Component from StorePicker</p>;
+    /* can't return sibling elements.
+    Workaround:
+    <React.Fragment>
+    <p></p>
+    <h1></h1>
+    </React.Fragment> */
+
+    //By Default only return one parent elements, can have multiple child elements of that parent elements
+    return (
+      <form className="store-selector">
+        <h2>Please Enter a store</h2>
+        <input type="text" required placeholder="Enter your Store name" />
+        <button type="submit">Visit Store</button>
+      </form>
+    );
   }
 }
 
