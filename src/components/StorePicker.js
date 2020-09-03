def8545e7de97771245234392730be998ae84946
SumnanAzadi <anadaplay@gmail.com>
2020-09-03 09:54:27 +0600

    First Basic Component StorePicker

---
+++ src/components/StorePicker.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class StorePicker extends React.Component {
+  //render: what DOM elements do I render to the html page
+  render() {
+    return <p>Hello from first Component from StorePicker</p>;
+  }
+}
+
+export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-// let's go!
-alert("Hey Brother !!!");
+import React from "react"; //we want entire 'react' package
+import { render } from "react-dom"; //we want only need 'render' method 'react-dom' package
+
+//Components
+import StorePicker from "./components/StorePicker";
+
+//render(<p>Hey Brother</p>, document.querySelector("#main")); //render simple tag
+render(<StorePicker />, document.querySelector("#main")); //render whole components
